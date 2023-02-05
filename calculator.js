ec56d339e214f8b8e5fdfe73364ea56ea5492f5c
Kenny Saavedra <kennysaavedraaltuna@outlook.com>
2023-02-05 20:38:04 +0100

    first commit

---
+++ calculator.js
@@ -0,0 +1,12 @@
+const pantallaOperaciones = document.querySelector(".texto-operaciones");
+const pantallaResultado = document.querySelector(".texto-resultado");
+const botones = document.querySelectorAll(".panel-boton");
+
+botones.forEach((boton) => {});
+
+var texto_evaluar = "",
+  texto_mostrar = "";
+
+function test() {
+  document.querySelector(".container").classList.toggle("dark-mode");
+}
